e7adea7438b067873d308608e60153fd6bd02804
NSQA <nsqa99@github.com>
2021-04-11 17:57:15 +0700

    root application file

---
+++ src/main/java/com/example/demo/DcmEclipseApplication.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import lombok.RequiredArgsConstructor;
+
+@SpringBootApplication()
+@RequiredArgsConstructor
+public class DcmEclipseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DcmEclipseApplication.class, args);
+	}
+
+}
