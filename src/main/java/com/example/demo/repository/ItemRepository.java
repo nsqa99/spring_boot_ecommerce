66f859892d9a304d102e0a77ed54a58c7ec188a5
NSQA <nsqa99@github.com>
2021-04-11 17:51:25 +0700

    data access layer

---
+++ src/main/java/com/example/demo/repository/ItemRepository.java
@@ -0,0 +1,14 @@
+package com.example.demo.repository;
+
+import javax.transaction.Transactional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import com.example.demo.model.Item;
+
+public interface ItemRepository extends JpaRepository<Item, Long> {
+	
+}
